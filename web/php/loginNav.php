7483a7bebedf7844c091746614fc9254ff510e54
jeanbible <50119147+jeanbible@users.noreply.github.com>
2019-05-03 14:55:00 -0400

    Update loginNav.php

---
+++ web/php/loginNav.php
@@ -0,0 +1,55 @@
+<?php
+  $loginNav="";
+  if ($_SERVER['PHP_SELF']=="/_h19/pfinal/projetfinal/web/index.php") {
+    if (isset($_SESSION['type']) == false) {
+?>
+    <ul>
+      <li><a href="php/connexion.php">Connexion</a></li>
+      <li><a href="php/signup.php">Inscription</a></li>
+    </ul>
+  <?php
+    }
+    else if (isset($_SESSION['type']) == "M") {
+  ?>
+      <ul>
+      <li><a href="php/deconnexion.php">Deconnexion</a></li>
+      <li><a href="signup.html"><?php echo $_SESSION['prenom']?></a></li>
+      </ul>
+  <?php
+    }
+    else if (isset($_SESSION['type']) == "A") {
+  ?>
+      <ul>
+        <li><a href="php/deconnexion.php">Deconnexion</a></li>
+        <li><a href="signup.html"><?php echo $_SESSION['prenom']?></a></li>
+      </ul>
+  <?php
+    }
+  }else{
+    if (isset($_SESSION['type']) == false) {
+  ?>
+      <ul>
+        <li><a href="connexion.php">Connexion</a></li>
+        <li><a href="signup.php">Inscription</a></li>
+      </ul>
+  <?php
+    }
+    else if (isset($_SESSION['type']) == "M") {
+  ?>
+      <ul>
+        <li><a href="deconnexion.php">Deconnexion</a></li>
+        <li><a href="signup.html"><?php echo $_SESSION['prenom']?></a></li>
+      </ul>
+  <?php
+    }
+    else if (isset($_SESSION['type']) == "A") {
+  ?>
+      <ul>";
+        <li><a href="deconnexion.php">Deconnexion</a></li>
+        <li><a href="signup.html"><?php echo $_SESSION['prenom']?></a></li>
+      </ul>
+  <?php
+    }
+  }
+
+ ?>
