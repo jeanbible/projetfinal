85acab21b2299c27764a6e933bc369ed817a8d03
David <davidccadieux@gmail.com>
2019-05-03 18:36:11 -0400

    Update config.js

--- web/ckeditor/config.js
+++ web/ckeditor/config.js
@@ -6,25 +6,5 @@
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-  config.toolbarGroups = [
-		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-		{ name: 'forms', groups: [ 'forms' ] },
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'colors', groups: [ 'colors' ] },
-		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-		'/',
-		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-		{ name: 'links', groups: [ 'links' ] },
-		{ name: 'insert', groups: [ 'insert' ] },
-		'/',
-		{ name: 'styles', groups: [ 'styles' ] },
-		{ name: 'tools', groups: [ 'tools' ] },
-		{ name: 'others', groups: [ 'others' ] },
-		{ name: 'about', groups: [ 'about' ] }
-	];
-
-	config.removeButtons = 'Source,Save,Cut,Copy,Paste,Replace,Checkbox,Form,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Subscript,Superscript,Strike,RemoveFormat,CopyFormatting,JustifyLeft,JustifyCenter,JustifyRight,JustifyBlock,BidiLtr,BidiRtl,Language,Anchor,Flash,Smiley,PageBreak,Iframe,Styles,Format,Font,FontSize,BGColor,ShowBlocks,About,NewPage,Print,Preview,Templates,Scayt,Blockquote,Outdent,Indent,CreateDiv,HorizontalRule,Maximize,PasteFromWord,PasteText,Find,SelectAll';
-
+	//config.uiColor = '#AADC6E';
 };
